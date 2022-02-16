6d719719de2359b70495bfe4503160d8ac9b5882
WesLemon <wesleyshaw25@gmail.com>
2022-02-16 17:43:56 +0000

    if-else-elif programs

--- udemy/hello.py
+++ udemy/hello.py
@@ -2,11 +2,11 @@
 
 print('Hello world!')
 
-print('What is your name?') # ask for their name
+print('What is your name?')  # ask for their name
 myName = input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is: ' + str(len(myName)))
 
-print('What is your age?') # ask for their age
+print('What is your age?')  # ask for their age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
